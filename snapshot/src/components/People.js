f013aabafa5e5d545912d6be08381bcd8d3851c1
KEVIN-DEVV <kyevenkims@gmail.com>
2023-01-26 16:32:23 +0300

    Added like functionalities

--- snapshot/src/components/People.js
+++ snapshot/src/components/People.js
@@ -1,23 +1,27 @@
 import React,{useState,useEffect}from 'react';
+import LikeButton from './LikeButton';
 
 function People(){
     const [images, setImages] = useState([]);
-    const [likes, setLikes] = useState([]);
+    const [likes, setLikes] = useState(Array(images.length).fill(0));
 
 
 
     const apikey = "2DKKpd6LevhrI90M8YszntYlBBGyR0iHnJ8NWpa46QZ0R8NXQUj6iJhc";
     const page_num = 100;
     useEffect(() => {
-        fetch(`https://api.pexels.com/v1/search?query=people&per_page=${page_num}`, {
-          headers: {
-            Authorization: apikey
-          }
+      fetch(`https://api.pexels.com/v1/search?query=nature&per_page=${page_num}`, {
+        headers: {
+          Authorization: apikey
+        }
+      })
+        .then(response => response.json())
+        .then(data => {
+          setImages(data.photos);
+          setLikes(Array(data.photos.length).fill(0));
         })
-          .then(response => response.json())
-          .then(data => setImages(data.photos))
-          .catch(error => console.log(error));
-      }, [page_num]);
+        .catch(error => console.log(error));
+    }, [page_num]);
 
       const handleLikeClick = (index) => {
         setLikes(prevLikes => {
@@ -39,7 +43,8 @@ return(
                 <img className="card-img-top" src={image.src.landscape} alt={image.photographer} />
                 <div className="card-body">
                   <p className="card-text">{image.photographer}</p>
-                  <button className="btn btn-danger" onClick={() => handleLikeClick(index)}>&#x2764; {likes[index]}</button>
+                 
+                  <LikeButton count={likes[index]} onClick={() => handleLikeClick(index)} />
 
                 </div>
               </div>
