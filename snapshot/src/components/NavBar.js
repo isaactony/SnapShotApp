d2e17142e50a23a14f254525aa4a7110e782e145
Isaac Tonyloi <59682377+isaactony@users.noreply.github.com>
2023-01-27 10:23:40 +0300

    Update NavBar.js

--- snapshot/src/components/NavBar.js
+++ snapshot/src/components/NavBar.js
@@ -1,5 +1,6 @@
-import React, { useState, useEffect } from 'react';
+import React,{useState, useEffect} from 'react';
 import { NavLink } from 'react-router-dom';
+import Card from './Card';
 
 
 function NavBar(){
@@ -7,41 +8,41 @@ function NavBar(){
     const [images, setImages] = useState([]);
     const [likes, setLikes] = useState([]);
 
-    const api_key = "2DKKpd6LevhrI90M8YszntYlBBGyR0iHnJ8NWpa46QZ0R8NXQUj6iJhc";
+    const apikey = "CrTWcyntot24vIxCoYojcEP9lfAXBcf8X2d3v7psz0V9qMMUNsN9X0NS";
 
     useEffect(() => {
         const handleSearch = async () => {
-          const response = await fetch(`https://api.pexels.com/v1/search?query=${searchInput}&per_page=15`, {
+          const response = await fetch(`https://api.pexels.com/v1/search?query=${searchInput}&per_page=20`, {
             headers: {
-              Authorization: api_key
+              Authorization: apikey
             }
           });
           const data = await response.json();
           setImages(data.photos);
+          setLikes(Array(data.photos.length).fill(0));
         }
         if (searchInput.length > 2) {
           handleSearch();
         }
       }, [searchInput]);
-      const handleLikeClick = (index) => {
+
+
+    const handleLikeClick = (index) => {
         setLikes(prevLikes => {
           const newLikes = [...prevLikes];
           newLikes[index]++;
           return newLikes;
         });
       }
-    
-    
-    
-  
-    
-    return(
+
+      return(
         <>
         <nav className="navbar navbar-expand-lg navbar-light text-white d-flex align-items-center justify-content-center">
             <div className='container-fluid d-flex flex-column align-items-center justify-content-center'>
                 <h1 className="font-weight-bold text-center my-4">SNAPSHOT APP</h1>
                 <div className='d-flex align-items-center justify-content-center my-4'>
-                  <NavLink style={{marginRight: '70px', textDecoration: 'none'}} to='/'>Gallery</NavLink>
+                <NavLink style={{marginRight: '70px', textDecoration: 'none'}} to='/'>Home</NavLink>
+                  <NavLink style={{marginRight: '70px', textDecoration: 'none'}} to='gallery'>Gallery</NavLink>
                   <NavLink style={{marginRight: '70px', textDecoration: 'none'}} to='/people'>People</NavLink>
                   <NavLink style={{marginRight: '70px', textDecoration: 'none'}} to='/cities'>Cities</NavLink>
                   <NavLink style={{marginRight: '70px', textDecoration: 'none'}} to='/animals'>Animals</NavLink>
@@ -49,35 +50,30 @@ function NavBar(){
                 </div>
                 <form  value={searchInput} onChange={(e) => setSearchInput(e.target.value)} className="d-flex" role="search">
                     <input className="form-control me-2 form-control-sm" type="search" placeholder="Search..." aria-label="Search"/>
-                    <button onClick={() => searchInput(searchInput)} className="btn btn-outline-success" type="submit">Search</button>
+                    
                 </form>
-                <div>
-                  </div>
-          
-          </div>
-          </nav>
-
-          <div className="image-grid-container"> 
-      <div className="image-grid">
-        <div className="row">
-          {images.map((image, index) => (
-            <div key={image.id} className="col-md-2">
-              <div className="card">
-                <img className="card-img-top" src={image.src.landscape} alt={image.photographer} />
-                <div className="card-body">
-                  <p className="card-text">{image.photographer}</p>
-                  <button className="btn btn-danger" onClick={() => handleLikeClick(index)}>&#x2764; {likes[index]}</button>
-
-                </div>
-              </div>
             </div>
-          ))}
-        </div>
-      </div>
-    </div>
-    </>
-          
+</nav>
+
+<div className="image-grid-container"> 
+<div className="image-grid">
+  <div className="row">
+  {images.map((image, index) => (
+        <Card 
+          key={index} 
+          image={image} 
+          index={index} 
+          handleLikeClick={handleLikeClick} 
+          likes={likes} 
+        />
+      ))}
+  </div>
+</div>
+</div>
+
+</>
+
     )
 }
 export default NavBar;
-
+    
